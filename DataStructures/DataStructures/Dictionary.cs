58c8d16123b072aed1d5eec3dc3c11e9d52c1c27
ardumez <arnauld.dumez@gmail.com>
2018-07-10 09:38:45 +0200

    Add dictionary

---
+++ DataStructures/DataStructures/Dictionary.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DataStructures
+{
+    public class Dictionary<TKey, TValue>
+    {
+        private object[] myObject;
+
+        public Dictionary(int size)
+        {
+            myObject = new object[size];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            myObject[GetHashPosition(key)] = value;
+        }
+
+        public void Contains(TKey key)
+        {
+
+        }
+
+        public int GetHashPosition(TKey key)
+        {
+            return key.GetHashCode() % 1000;
+        }
+
+    }
+}
